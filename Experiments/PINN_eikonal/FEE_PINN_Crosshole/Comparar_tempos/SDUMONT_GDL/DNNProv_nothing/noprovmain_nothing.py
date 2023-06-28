45650e4e9c2db2da899e2425683cf958abc544d5
Lyncoln <lyncolnsousa@id.uff.br>
2023-06-28 14:24:11 -0300

    Comparar tempos

---
+++ Experiments/PINN_eikonal/FEE_PINN_Crosshole/Comparar_tempos/SDUMONT_GDL/DNNProv_nothing/noprovmain_nothing.py
@@ -0,0 +1,125 @@
+from noprovutils_nothing import EikonalSolver2D
+import numpy as np
+import tensorflow as tf
+import argparse
+import os
+from datetime import datetime
+
+
+import sys
+
+""" Parser """
+
+
+
+
+def verifica_experimento(string):
+	if(string=="Exp_1"):
+	    element={"path":"Exp_1","prov_opt":"Adam","prov_act_tt":"tanh","prov_act_vel":"tanh","layers_tt":[4]+8*[20]+[1],"layers_vel":[2]+4*[32]+[1]}
+	    
+	elif(string=="Exp_2"):
+	    element={"path":"Exp_2","prov_opt":"Adam","prov_act_tt":"relu","prov_act_vel":"relu","layers_tt":[4]+8*[20]+[1],"layers_vel":[2]+4*[32]+[1]}
+	    
+	elif(string=="Exp_3"):
+	    element={"path":"Exp_3","prov_opt":"Adam","prov_act_tt":"sigmoid","prov_act_vel":"sigmoid","layers_tt":[4]+8*[20]+[1],"layers_vel":[2]+4*[32]+[1]}
+	    
+	elif(string=="Exp_4"):
+	    element={"path":"Exp_4","prov_opt":"Adam","prov_act_tt":"relu","prov_act_vel":"tanh","layers_tt":[4]+8*[20]+[1],"layers_vel":[2]+4*[32]+[1]}
+	    
+	elif(string=="Exp_5"):
+	    element={"path":"Exp_5","prov_opt":"RMSProp","prov_act_tt":"tanh","prov_act_vel":"tanh","layers_tt":[4]+8*[20]+[1],"layers_vel":[2]+4*[32]+[1]}
+	    
+	elif(string=="Exp_6"):
+	    element={"path":"Exp_6","prov_opt":"Adam","prov_act_tt":"tanh","prov_act_vel":"tanh","layers_tt":[4]+16*[40]+[1],"layers_vel":[2]+8*[64]+[1]}
+	    
+	elif(string=="Exp_7"):
+	    element={"path":"Exp_7","prov_opt":"RMSProp","prov_act_tt":"tanh","prov_act_vel":"tanh","layers_tt":[4]+16*[40]+[1],"layers_vel":[2]+8*[64]+[1]}
+	    
+	elif(string=="Exp_8"):
+	    element={"path":"Exp_8","prov_opt":"Adam","prov_act_tt":"tanh","prov_act_vel":"tanh","layers_tt":[4]+16*[20]+[1],"layers_vel":[2]+8*[32]+[1]}
+	    
+	elif(string=="Exp_9"):
+	    element={"path":"Exp_9","prov_opt":"RMSProp","prov_act_tt":"tanh","prov_act_vel":"tanh","layers_tt":[4]+16*[20]+[1],"layers_vel":[2]+8*[32]+[1]}
+	    
+	return element
+    
+    
+local = os.getcwd() + '/mnt/Potato/Experimentos'
+
+
+
+#path = element["path"]
+#local_save = local + '/' + path
+local_save = local + "/" + datetime.today().strftime('%Y%m%d_%H%M%S')
+
+
+parser = argparse.ArgumentParser(description="Eikonal Problem")
+
+parser.add_argument('--exp', type=str,default ="")
+
+# Adam its
+parser.add_argument('--adam_its', type=int, default=400000)
+#parser.add_argument('--adam_its', type=int, default=4000)
+
+# Number of grid points (in each dimension)
+parser.add_argument('--nx', type=int, default=10*20)
+
+parser.add_argument('--ny', type=int, default=10*25)
+
+if not os.path.exists(local_save):
+	os.mkdir(local_save)
+
+parser.add_argument('--folder', type=str, default=local_save)
+
+# Batch size
+parser.add_argument('--batchsize', type=int, default=3000)
+
+# Arguments
+args = parser.parse_args()
+
+element = verifica_experimento(args.exp)
+
+""" Preprocess the data """
+data_np = np.load('../../../data/crosshole_data.npz')
+
+pp_data = {'xs': data_np['datax'][:, 0:1],
+       'ys': data_np['datax'][:, 1:2],
+       'xr': data_np['datax'][:, 2:3],
+       'yr': data_np['datax'][:, 3:4],
+       'tt': data_np['datay'][:, 0:1],
+       'sp_x': data_np['sources'][:, 0:1],
+       'sp_y': data_np['sources'][:, 1:2]}
+
+""" Set the solver """
+
+
+# layers_vel = [2] + 4 * [32] + [1]
+# layers_tt = [4] + 8 * [20] + [1]
+
+
+
+path = element["path"]
+
+eikonal_solver = EikonalSolver2D(layers_vel=element["layers_vel"],
+                             layers_tt=element["layers_tt"],
+                             batch_size=args.batchsize,
+                             vmin=1.5, vmax=4.0,
+                             xmin=0, xmax=20,
+                             ymin=-27.5, ymax=0,
+                             data=pp_data,
+                             base_folder=args.folder,
+                             adam_its=args.adam_its,
+                             prov_opt = element["prov_opt"],
+                             prov_act_tt = element["prov_act_tt"],
+                             prov_act_vel = element["prov_act_vel"],
+                             path = path,
+                             weight_lb = 1,
+                             weight_lr = 1,
+                             weight_ld = 25)
+
+""" Train the model """
+
+
+eikonal_solver.train(Nx=args.nx, Ny=args.ny)
+    
+    
